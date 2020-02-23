2b5e253e58d578fe070d53ec9382eef9be7ab55d
shonenada <shonenada@gmail.com>
2020-02-23 19:21:34 +0800

    feat: impl Base64.encode

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,7 +1,8 @@
 export { wasiabort } from './env';
 
 import { Console } from '../node_modules/as-wasi/assembly';
+import { Base64 } from './base64';
 
 export function _start(): void {
-    Console.log('First blood');
+    Console.log('Base64Encode("First blood") = ' + Base64.encode("First blood"));
 }
