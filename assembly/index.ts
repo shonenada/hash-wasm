c0e2480055a7ff9e742bcb39e30e1331db12cf88
shonenada <shonenada@gmail.com>
2020-02-25 02:18:44 +0800

    feat: Helper functions for passing data from/to wasmer

--- assembly/index.ts
+++ assembly/index.ts
@@ -11,6 +11,43 @@ function showHelp(): void {
     Console.log("");
 }
 
+let memory = new ArrayBuffer(1024);
+export function getMemoryPtr(): usize {
+    return changetype<usize>(memory);
+}
+
+function loadStringFromMemory(len: usize): string {
+    let view = new DataView(memory);
+    return String.UTF8.decode(view.buffer, true);
+}
+
+function saveStringIntoMemory(raw: string): usize {
+    const len = raw.length;
+    let view = new DataView(memory);
+    for (let i=0;i<(len as i32);i++) {
+        view.setUint8(i, raw.charCodeAt(i) as i8);
+    }
+    return len;
+}
+
+export function printMemory(len: usize): usize {
+    const string = loadStringFromMemory(len);
+    Console.log(string);
+    return len;
+}
+
+export function b64encode(len: usize): usize {
+    const inputString = loadStringFromMemory(len);
+    const encoded = Base64.encode(inputString);
+    return saveStringIntoMemory(encoded);
+}
+
+export function b64decode(len: usize): usize {
+    const inputString = loadStringFromMemory(len);
+    const encoded = Base64.decode(inputString);
+    return saveStringIntoMemory(encoded);
+}
+
 export function _start(): void {
     const cli = new CommandLine();
     const args: Array<string> = cli.all();
@@ -20,6 +57,7 @@ export function _start(): void {
         showHelp();
         return;
     }
+
     let method = args[1];
     if (method.toLowerCase() == 'b64encode') {
         if (argsLength <= 2) {
